39685682ead9570ed2841bce552d582114c53104
Sam Smale <samsmale@Sams-MacBook-Pro.local>
2015-08-30 16:29:49 -0400

    seeds

--- random_adv/db/seeds.rb
+++ random_adv/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Adventure.create({location: 'ChIJtcaxrqlZwokRfwmmibzPsTU', price_per_person: false, price_event: 0, timeframe: 2, hints: "King Kong's jungle gym"})
+Adventure.create({location: 'ChIJ-4MbiBlawokR5ixJ4E8Km5c', price_per_person: false, price_event: 0, timeframe: 1, hints: "Freedom tower"})
+Adventure.create({location: 'ChIJlRYp-4xYwokRqtwtQBYVz80', price_per_person: false, price_event: 0, timeframe: 1, hints: "Strawberry fields"})
+Adventure.create({location: 'ChIJmQJIxlVYwokRLgeuocVOGVU', price_per_person: false, price_event: 0, timeframe: 1, hints: "Times Square"})
+Adventure.create({location: 'ChIJK3vOQyNawokRXEa9errdJiU', price_per_person: false, price_event: 0, timeframe: 2, hints: "Brooklyn bridge"})
+Adventure.create({location: 'ChIJ65rH6TBawokRUuhYOuefE28', price_per_person: false, price_event: 0, timeframe: 3, hints: "Smorgasburg"})
+Adventure.create({location: 'ChIJYZrvCIFZwokRW3Xf3V0BWnU', price_per_person: true, price_event: 28, timeframe: 2, hints: "Escape the Room Downtown"})
+Adventure.create({location: 'ChIJHW8b6qhZwokRJtwCEu3NV3w', price_per_person: true, price_event: 28, timeframe: 2, hints: "Escape the Room Midtown"})
+Adventure.create({location: 'ChIJ5bQPhMdZwokRkTwKhVxhP1g', price_per_person: false, price_event: 0, timeframe: 1, hints: "High Line"})
+Adventure.create({location: 'ChIJNZvR1v5YwokRFHSjCAPwtIs', price_per_person: true, price_event: 30, timeframe: 2, hints: "Top of The Rock"})
+Adventure.create({location: 'ChIJ9QDTS7pZwokRJCPIsMF2Kiw', price_per_person: false, price_event: 45, timeframe: 1, hints: "Chelsea Driving Range Simulator"})
+Adventure.create({location: 'ChIJaQ2FO55ZwokRKYAlmF0IlzA', price_per_person: false, price_event: 0, timeframe: 2, hints: "Crocodile lounge"})
+Adventure.create({location: 'ChIJkXQ3_INZwokRE74ZvG46Jjc', price_per_person: false, price_event: 0, timeframe: 2, hints: "Momofuko noodle bar"})
+Adventure.create({location: 'ChIJFTweVbZZwokRNkSu_ncOwJs', price_per_person: true, price_event: 85, timeframe: 2, hints: "Sleep no more"})
+
+
