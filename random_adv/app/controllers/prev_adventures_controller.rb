ab1e2b37890cf6a756b9b2b1bd763884582b6d25
Matthew Yeung <csyeung87@gmail.com>
2015-08-31 15:55:29 -0400

    committing

--- random_adv/app/controllers/prev_adventures_controller.rb
+++ random_adv/app/controllers/prev_adventures_controller.rb
@@ -3,9 +3,11 @@ class PrevAdventuresController < ApplicationController
 	def index
 		# views user's past adventures
 		@current_user = User.find(session[:user_id])
+		@prev_adven = Prev_Adventure.where(@current_user.id)
 		if logged_in? && check_current_user? 
 			@current_user = User.find(session[:user_id])
-			@prev_adven = Prev_Aventure.find(session[:user_id])
+			# @prev_adven = Prev_Adventure.find(session[:user_id])
+		# binding.pry
 		else
 			# redirect_to user_path(@current_user)
 		end
--- random_adv/app/models/prev_adventure.rb
+++ random_adv/app/models/prev_adventure.rb
@@ -1,3 +1,4 @@
 class Prev_Adventure < ActiveRecord::Base
 	belongs_to :user
+	belongs_to :adventure
 end
