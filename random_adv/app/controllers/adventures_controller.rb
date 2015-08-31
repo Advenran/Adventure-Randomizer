9bac876e3b05981846bf60c3d2a8d8ffbcc95c5c
Matthew Yeung <csyeung87@gmail.com>
2015-08-31 13:40:31 -0400

    weather api

--- random_adv/app/controllers/adventures_controller.rb
+++ random_adv/app/controllers/adventures_controller.rb
@@ -5,16 +5,25 @@ class AdventuresController < ApplicationController
 	include HTTParty
 
 	def new
-		#Wunderground weather API
-		response = HTTParty.get("http://api.wunderground.com/api/abc3ed8ecb8d84a6/conditions/q/NY/new_york.json")
-		temperature = response["current_observation"]["temp_f"]
-		weather = response["current_observation"]["weather"]
-		icon = response["current_observation"]["icon_url"]
-		current_forecast = "the current weather for your adventure is #{weather} and #{temperature}!"
-		# to test
-		puts "the current weather for your adventure is #{weather} and #{temperature}F!"
+		actual_user = User.find(session[:user_id])
+		@current_user = User.find(session[:user_id])
+			#Wunderground weather API
+			response = HTTParty.get("http://api.wunderground.com/api/abc3ed8ecb8d84a6/conditions/q/NY/new_york.json")
+			temperature = response["current_observation"]["temp_f"]
+			weather = response["current_observation"]["weather"]
+			icon = response["current_observation"]["icon_url"]
+			@current_forecast = "the current weather for your adventure is #{weather} and #{temperature}!"
+			# to test
+			puts "the current weather for your adventure is #{weather} and #{temperature}F!"
 
+		#grab the affairs for the user accessing the route and pass them into a template
+		if logged_in? && check_current_user? #this is our definition of logged in
+			@current_user = User.find(session[:user_id])
+		else
+				# redirect_to user_path(actual_user)
+		end
 	end
+		
 
 
 end
