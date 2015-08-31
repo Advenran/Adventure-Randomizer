0e71be8ea98be27b88f44a83cc3eb15590967fa4
Ethan Schiavi <etschiavi@gmail.com>
2015-08-31 15:53:06 -0400

    committing

--- random_adv/app/controllers/adventures_controller.rb
+++ random_adv/app/controllers/adventures_controller.rb
@@ -5,18 +5,36 @@ class AdventuresController < ApplicationController
 	include HTTParty
 
 	def new
-		#Wunderground weather API
-		response = HTTParty.get("http://api.wunderground.com/api/abc3ed8ecb8d84a6/conditions/q/NY/new_york.json")
-		temperature = response["current_observation"]["temp_f"]
-		weather = response["current_observation"]["weather"]
-		icon = response["current_observation"]["icon_url"]
-		current_forecast = "the current weather for your adventure is #{weather} and #{temperature}!"
-		# to test
-		puts "the current weather for your adventure is #{weather} and #{temperature}F!"
+		actual_user = User.find(session[:user_id])
+		@current_user = User.find(session[:user_id])
+			#Wunderground weather API
+			response = HTTParty.get("http://api.wunderground.com/api/abc3ed8ecb8d84a6/conditions/q/NY/new_york.json")
+			temperature = response["current_observation"]["temp_f"]
+			weather = response["current_observation"]["weather"]
+			icon = response["current_observation"]["icon_url"]
+			@current_forecast = "the current weather for your adventure is #{weather} and #{temperature}!"
+			# to test
+			puts "the current weather for your adventure is #{weather} and #{temperature}F!"
+
+
+		if logged_in? && check_current_user? #this is our definition of logged in
+			@current_user = User.find(session[:user_id])
+			#below is the code to generate a random adventure
+		else
+				# redirect_to user_path(actual_user)
+		end
 
 		@adventure = Adventure.new
 
+
 	end
+
+	def create
+		@adventure_choice = Adventure.where[rand(Adventure.all.length)]
+
+
+	end
+		
 
 
 end
