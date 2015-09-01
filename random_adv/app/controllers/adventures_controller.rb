113722d80daf30b99bdea7b48213a9d88fd4f8a4
Sam Smale <samsmale@Sams-MacBook-Pro.local>
2015-09-01 09:18:07 -0400

    working on google maps

--- random_adv/app/controllers/adventures_controller.rb
+++ random_adv/app/controllers/adventures_controller.rb
@@ -21,6 +21,7 @@ class AdventuresController < ApplicationController
 		if logged_in? && check_current_user? #this is our definition of logged in
 			@current_user = User.find(session[:user_id])
 			#below is the code to generate a random adventure
+
 		else
 				# redirect_to user_path(actual_user)
 		end
@@ -30,6 +31,22 @@ class AdventuresController < ApplicationController
 
 	end
 
+	def show
+
+		adventure_choice = params[:id]
+
+		response = HTTParty.get("https://maps.googleapis.com/maps/api/directions/json?origin=place_id:ChIJaQ2FO55ZwokRKYAlmF0IlzA&destination=place_id:ChIJa1QZIAJZwokRySwnNKYyMno&key=AIzaSyAHag6dnSTJnXF7Kg-le6dNEqsTVZesl_o")
+
+		@directions = response["routes"][0]["legs"][0]["steps"]
+
+		puts adventure_choice
+
+	
+
+
+	end
+
+
 	def create
 		#timeframe selected
 		amount_time = params[:amount_time]
@@ -38,8 +55,9 @@ class AdventuresController < ApplicationController
 		#amount selected
 		amount_spend = params[:amount_spend]
 		#adventure.where({})
-		@adventure_choice = Adventure.where({})
+		adventure_choice = Adventure.where(":timeframe < ?", 150)
 
+		puts adventure_choice
 
 
 		#grab the affairs for the user accessing the route and pass them into a template
