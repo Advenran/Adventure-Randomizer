81f1db7650badf82b9d27547a4efc7439c7863ec
Sam Smale <samsmale@Sams-MacBook-Pro.local>
2015-09-01 15:53:55 -0400

    it works

--- random_adv/app/controllers/sessions_controller.rb
+++ random_adv/app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-
+	include HTTParty
 	def new
 	end
 
--- random_adv/config/routes.rb
+++ random_adv/config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   post '/sessions' => 'sessions#create'
   delete '/sessions' => 'sessions#destroy'
 
+  post '/users/:user_id/prev_adventures/new' => 'prev_adventures#create'
+
 
   resources :users do 
     resources :adventures
-    resources :prev_adventures, only: [:index, :new, :create]
+    resources :prev_adventures
   end
 
 
