f2e3b6908c371eccf1e6503dae0418489099a90c
Ethan Schiavi <etschiavi@gmail.com>
2015-09-02 09:27:19 -0400

    updated databases to inlude

--- random_adv/app/controllers/sessions_controller.rb
+++ random_adv/app/controllers/sessions_controller.rb
@@ -8,6 +8,23 @@ class SessionsController < ApplicationController
 		user = User.find_by({email: params[:email]})
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
+
+			def success(pos)
+				crd = pos.coords;
+
+				puts 'Your current position is:'
+				puts 'Latitude : ' + crd.latitude
+				puts 'Longitude: ' + crd.longitude
+				puts 'More or less ' + crd.accuracy + ' meters.'
+			end
+
+			def error(err)
+			  puts warn('ERROR(' + err.code + '): ' + err.message);
+			end
+
+			# navigator.geolocation.getCurrentPosition(success, error, options);
+
+
 			redirect_to new_user_prev_adventure_path(user)
 		else
 			redirect_to sessions_new_path
