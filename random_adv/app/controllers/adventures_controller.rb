c56f95e991612ebf3da91297540e6251ea9978a3
Ethan Schiavi <etschiavi@gmail.com>
2015-09-02 11:15:09 -0400

    updated adv controller

--- random_adv/app/controllers/adventures_controller.rb
+++ random_adv/app/controllers/adventures_controller.rb
@@ -2,7 +2,8 @@ require "HTTParty"
 
 class AdventuresController < ApplicationController
 
-	def index	
+	def index
+		#index not needed
 	end
 
 	def new
@@ -22,53 +23,36 @@ class AdventuresController < ApplicationController
 		else
 			redirect_to user_path(actual_user)
 		end
-
 	end
 
 	def show
-		#for rendering page with directions
+		#env variable key and HTTP response for Google API
+		api_key = ENV["WING_IT_GOOGLE"]
+		response = HTTParty.get("https://maps.googleapis.com/maps/api/directions/json?origin=place_id:ChIJT3jEwaNZwokRS-hniJsDhDg&destination=place_id:#{end_location}&mode=walking&key=#{api_key}")
+		
+		#for rendering page with their destination directions
 		@adventure_choice = Adventure.find(params[:id])
 		end_location = @adventure_choice.location
 		@hint = @adventure_choice.hints
 		@current_user = User.find(params[:user_id])
-		api_key = ENV["WING_IT_GOOGLE"]
-
-		response = HTTParty.get("https://maps.googleapis.com/maps/api/directions/json?origin=place_id:ChIJT3jEwaNZwokRS-hniJsDhDg&destination=place_id:#{end_location}&mode=walking&key=#{api_key}")
-
+		@total_duration = response["routes"][0]["legs"]["duration"]
 		@directions = response["routes"][0]["legs"][0]["steps"]
-
 	end
 
 
 	def create
-		#timeframe selected
+		#gathering page information
 		amount_time = params[:amount_time]
-		#people selected
 		amount_people = params[:amount_people]
-		#amount selected
 		amount_spend = params[:amount_spend]
-
 		@user_id = params[:user_id]
-
 		@current_user = User.find(session[:user_id])
 
-
-		#grab the affairs for the user accessing the route and pass them into a template
 		if logged_in? && check_current_user? #this is our definition of logged in
 			@current_user = User.find(session[:user_id])
-
-			
-			#logic for randomizer
-			# @new_array = Adventure.where("timeframe <= ? AND per_person <= ?", amount_time, amount_spend)
-			# @new_adventure = @new_array.sample
-			# @new_prev_adventure = @new_adventure.prev_adventures
-
-			# puts @new_adventure
-			# puts @new_prev_adventure
-
 			redirect_to user_adventure_path(adventure)
 		else
-				# redirect_to user_path(actual_user)
+			redirect_to user_path(actual_user)
 		end
 	end
 		
