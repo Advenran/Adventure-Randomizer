55adaea102dc808f2ead6ccc86c560dc57d8401a
Matthew Yeung <csyeung87@gmail.com>
2015-09-02 14:02:25 -0400

    fixed update user

--- random_adv/app/models/user.rb
+++ random_adv/app/models/user.rb
@@ -2,7 +2,4 @@ class User < ActiveRecord::Base
 	has_secure_password
 	has_many :prev_adventures
 	has_many :adventures, through: :prev_adventures
-	geocoded_by :ip_address,
-  :latitude => :lat, :longitude => :lon
-after_validation :geocode
 end
